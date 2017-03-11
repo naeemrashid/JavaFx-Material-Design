8477aa8aa2238c3e3ca9ff42e7167bf0b0009307
naeemkhan12 <naeemb7070@gmail.com>
2017-03-11 22:39:41 +0400

    button material with jfoenix.

---
+++ src/javaFxButton/Comtroller.java
@@ -0,0 +1,27 @@
+package javaFxButton;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+public class Comtroller implements Initializable{
+	  @FXML
+	    private JFXButton greenButton;
+
+	    @FXML
+	    private JFXButton yellowButton;
+
+	    @FXML
+	    private JFXButton redButton;
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
---
+++ src/javaFxButton/Main.java
@@ -0,0 +1,23 @@
+package javaFxButton;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("ButtonGui.fxml"));
+		Scene scene = new Scene(root);
+		stage.setScene(scene);
+		stage.show();
+		
+	}
+	public static void main(String[] args){
+		launch(args);
+	}
+
+}
