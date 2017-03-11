4d87c58686ce25a87a1642ce385885ccda464212
naeemkhan12 <naeemb7070@gmail.com>
2017-03-11 23:13:19 +0400

    TextField MaterialDesign

---
+++ src/textField/Controller.java
@@ -0,0 +1,59 @@
+package textField;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.validation.RequiredFieldValidator;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller implements Initializable{
+	@FXML
+    private JFXTextField nameField;
+
+    @FXML
+    private JFXTextField jobField;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		RequiredFieldValidator validator = new RequiredFieldValidator();
+		nameField.getValidators().add(validator);
+		jobField.getValidators().add(validator);
+		validator.setMessage("NO INPUT GIVEN.");
+		nameField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if(!newValue){
+					nameField.validate();
+				}
+			}
+		});
+		jobField.focusedProperty().addListener(new ChangeListener<Boolean>() {
+
+			@Override
+			public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+				if(!newValue){
+					jobField.validate();
+				}
+			}
+		});
+		
+		try {
+			Image icon = new Image(new FileInputStream("E:/workspace/JavaFxMaterialDesign/src/textField/cross.jpg"));
+			validator.setIcon(new ImageView(icon));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
