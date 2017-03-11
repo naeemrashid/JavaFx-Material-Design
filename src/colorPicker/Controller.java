60deadccc37a9aaab23b0e77319e8638ab06a002
naeemkhan12 <naeemb7070@gmail.com>
2017-03-11 23:54:24 +0400

    Material Design ColorPicker with jfoenix.

---
+++ src/colorPicker/Controller.java
@@ -0,0 +1,46 @@
+package colorPicker;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXColorPicker;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
+
+public class Controller implements Initializable{
+
+    @FXML
+    private JFXCheckBox flag;
+	@FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private Pane pane;
+	  @FXML
+	    private JFXColorPicker colorPicker;
+
+	    @FXML
+	    void changeColor(ActionEvent event) {
+	    	if(flag.isSelected()){
+	    		Color selectedColor = colorPicker.getValue();
+		    	anchorPane.setBackground(new Background(new BackgroundFill(Paint.valueOf(selectedColor.toString()), CornerRadii.EMPTY, Insets.EMPTY)));
+
+	    	}
+	    	
+	    }
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+	}
+
+}
---
+++ src/colorPicker/Main.java
@@ -0,0 +1,24 @@
+package colorPicker;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+			Parent root = FXMLLoader.load(getClass().getResource("ColorPick.fxml"));
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+			
+		}
+
+}
